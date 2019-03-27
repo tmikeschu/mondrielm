fbf3c3540fcd58daf1ad9f018597829cfa919d9b
Mike Schutte <schutte@clinc.com>
2019-03-27 09:08:18 -0400

    Remove example failing test

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,7 +1,8 @@
-module Tests exposing (..)
+module Tests exposing (all)
 
-import Test exposing (..)
 import Expect
+import Test exposing (..)
+
 
 
 -- Check out http://package.elm-lang.org/packages/elm-community/elm-test/latest to learn more about testing in Elm!
@@ -13,10 +14,4 @@ all =
         [ test "Addition" <|
             \_ ->
                 Expect.equal 10 (3 + 7)
-        , test "String.left" <|
-            \_ ->
-                Expect.equal "a" (String.left 1 "abcdefg")
-        , test "This test should fail" <|
-            \_ ->
-                Expect.fail "failed as expected!"
         ]
